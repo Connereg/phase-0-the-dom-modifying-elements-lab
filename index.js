32c051af2cebf7c86f132078af9403a52918a5fc
Conner Gleason <connereg@optonline.net>
2021-07-28 15:47:49 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainGot = document.getElementById('main');
+mainGot.remove();
+
+const createVictory = document.createElement('h1');
+document.body.appendChild(createVictory);
+createVictory.className = "victoryClass";
+createVictory.id = "victory";
+
+const newHeader = document.getElementById("victory");
+newHeader.innerHTML = "<h1>Conner is the champion</h1>";
